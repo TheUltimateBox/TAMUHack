b4445da7f10a7f573f2db5e57059028b0683eb26
sean <seanchoi@tamu.edu>
2019-01-26 17:26:09 -0600

    part 3

--- TAMUHACKFINALE/app/views.py
+++ TAMUHACKFINALE/app/views.py
@@ -3,6 +3,10 @@ from flask import render_template
 
 from app import app
 
+@app.route('/prelogin')
+def login():
+    return render_template("login.html")
+
 @app.route('/')
 def index():
     return render_template("index.html")
