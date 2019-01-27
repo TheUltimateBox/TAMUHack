f7a6f5646be4bdd13fb11d54a8d58fa2fac857c2
christinaaag <christina.gan5@tamu.edu>
2019-01-26 18:49:14 -0600

    working version

--- app/views.py
+++ app/views.py
@@ -9,7 +9,7 @@ def login():
 
 @app.route('/')
 def index():
-    return render_template("index.html")
+    return render_template("home.html")
 
 
 @app.route('/about')
@@ -20,9 +20,21 @@ def about():
 @app.route('/afterLogin')
 def loggedIn():
     return render_template("loggedIn.html")
-@app.route('/account')
-def account():
-    return render_template("about.html")
+
+
+@app.route('/preferences')
+def preferences():
+    return render_template("preferences.html")
+
+
+@app.route('/swipe')
+def swipe():
+    return render_template("swipe.html")
+
+
+@app.route('/signIn')
+def signIn():
+    return render_template("index.html")
 
 # @app.route('/aboutfooder')
 # def about():
